15c6a85b9f7bd1dcc54ba3e674cd9942e8d9b5a2
Hayden Boettcher <htboettcher@gmail.com>
2021-01-27 21:47:54 -0700

    Add ability to edit initiative after rolling

---
+++ scripts/addInlineInitiativeInput.js
@@ -0,0 +1,25 @@
+Hooks.on("renderCombatTracker", (app, html, data) => {
+  const currentCombat = data.combats[data.currentIndex - 1];
+  if (currentCombat) {
+    html.find(".combatant").each((i, el) => {
+      const combatantId = el.dataset.combatantId;
+      const combatant = currentCombat.data.combatants.find(
+        (c) => c._id === combatantId
+      );
+      const initiative = combatant.initiative;
+      if (initiative && combatant.owner) {
+        const initDiv = el.getElementsByClassName("token-initiative")[0];
+        initDiv.innerHTML = `<input id="${combatantId}-init-input" type="number" value="${
+          initiative ?? ""
+        }" style="color:white">`;
+        initDiv.addEventListener("change", async (e) => {
+          const inputElement = e.target;
+          await currentCombat.setInitiative(
+            combatantId,
+            inputElement.value ?? "-9999"
+          );
+        });
+      }
+    });
+  }
+});
